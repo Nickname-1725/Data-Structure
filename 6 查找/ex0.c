94e0433e430c5fb0d60265fe14097be5f3454ef9
Stupid-silly <61688147+Stupid-silly@users.noreply.github.com>
2020-07-12 15:51:22 +0800

    Add files via upload

---
+++ 6 查找/ex0.c
@@ -0,0 +1,187 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct student {
+    int sno;
+    char name[8];
+} Student;
+
+#define MAX_STUDENTS 32
+Student students[MAX_STUDENTS];
+int studentNumber = 0;
+
+void addStudent(int sno, char *name)
+{
+    // Student *student = &students[studentNumber];
+    Student *student = students + studentNumber;name = E
+name = F
+name = G
+>Exit code: 0
+>./ex0
+name = (null)
+name = B
+name = C
+name = D
+name = E
+name = F
+name = G
+>Exit code: 0
+>./ex0
+name = (null)
+name = B
+name = C
+name = D
+name = E
+name = F
+name = G
+>Exit code: 0
+>./ex0
+name = (null)
+name = B
+name = C
+name = D
+name = E
+name = F
+name = G
+>Exit code: 0
+>./ex0
+name = (null)
+name = B
+name = C
+name = D
+name = E
+name = F
+name = G
+>Exit code: 0
+>./ex0
+name = (null)
+name = B
+name = C
+name = D
+name = E
+name = F
+name = G
+>Exit code: 0
+>./ex0
+name = (null)
+name = B
+name = C
+name = D
+name = E
+name = F
+name = G
+>Exit code: 0
+>./ex0
+name = (null)
+name = B
+name = C
+name = D
+name = E
+name = F
+name = G
+>Exit code: 0
+    student->sno = sno;
+    strcpy(student->name, name);
+    studentNumber++;
+}
+
+void dumpStudent(Student *student)
+{
+    printf("sno = %d, name = %s\n", student->sno, student->name);
+}
+
+void dumpStudents()
+{
+    int i;
+
+    for (i = 0; i < studentNumber; i++) {
+        Student *student = students + i;
+        dumpStudent(student);
+    }
+}
+
+void createStudents()
+{
+    addStudent(1, "A");
+    addStudent(2, "B");
+    addStudent(3, "C");
+    addStudent(4, "D");
+    addStudent(5, "E");
+    addStudent(6, "F");
+    addStudent(7, "G");
+}
+
+char *linearSearch(int sno)
+{
+    int i;
+
+    for (i = 0; i < studentNumber; i++) {
+        Student *student = students + i;
+        if (student->sno == sno)
+            return student->name;
+    } 
+
+    return NULL;
+}
+
+void testLinearSearch()
+{
+    createStudents();
+
+    char *name;
+    name = linearSearch(3);
+    printf("name = %s\n", name);
+    name = linearSearch(5);
+    printf("name = %s\n", name);
+}
+
+// [low, high)
+char *binarySearch(int sno)
+{
+ //	int Search_Bin ( SSTable ST, Key key )
+//	{
+ //  low = 1;  high = ST.length;     // 置区间初值
+ //  while (low <= high) {
+ //     mid = (low + high) / 2;
+ //     if  (key == ST.elem[mid].key) 
+ //       return  mid;        // 找到待查元素
+//     else  if (key < ST.elem[mid].key) 
+//        high = mid - 1;       // 继续在前半区间进行查找
+//      else  low = mid + 1; // 继续在后半区间进行查找
+//   }
+	int low = 1 , high = studentNumber;
+	
+	while (low <= high)
+	{
+		int mid=( low + high ) / 2;
+		
+		if (sno == students[mid + 1].sno )
+			return students[mid].name;
+		else 
+			if(sno < students[mid].sno)
+				high = mid -1;
+			else
+				low = mid + 1 ;
+		
+	}
+	
+	return 0;                 // 顺序表中不存在待查元素
+} // Search_Bin   
+	
+void testBinarySearch()
+{
+    createStudents();
+
+    for (int sno = 1; sno <= 7; sno++) {
+        char *name;
+        name = binarySearch(sno);
+        printf("name = %s\n", name);
+    }
+}
+
+int main()
+{
+    testBinarySearch();
+    return 0;
+}
---
+++ 6 查找/tree.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+typedef struct tree {
+    int sno;
+    char name[8];
+    struct tree *left;
+    struct tree *right;
+} Tree;
+
+void treeDump(Tree *tree)
+{
+    printf("sno = %d, name = %s\n", tree->sno, tree->name);
+}
+
+Tree *treeNew(int sno, char *name, Tree *left, Tree *right)
+{
+    Tree *tree = malloc(sizeof(Tree));
+    tree->sno = sno;
+    strcpy(tree->name, name);
+    tree->left = left;
+    tree->right = right;
+    return tree;
+}
+
+//     4
+//  2     6
+// 1 3   5 7
+Tree *createTree()
+{
+    Tree *a = treeNew(1, "A", NULL, NULL);
+    Tree *c = treeNew(3, "C", NULL, NULL);
+    Tree *b = treeNew(2, "B", a, c);
+
+    Tree *f = treeNew(5, "E", NULL, NULL);
+    Tree *g = treeNew(7, "G", NULL, NULL);
+    Tree *e = treeNew(6, "F", f, g);
+
+    Tree *d = treeNew(4, "D", b, e);
+    return d;
+}
+
+void treeVisit(Tree *tree)
+{
+    if (tree != NULL) {
+        treeVisit(tree->left);
+        treeDump(tree);
+        treeVisit(tree->right);
+    }
+}
+
+char *treeSearch(Tree *root, int sno)
+{
+    if(sno==root->sno)
+	{
+		return root->name;
+	}
+	else if(sno > root -> sno && root->right)
+		return treeSearch(root->right,sno);
+	
+	else if(sno < root -> sno && root->left)
+		return treeSearch(root->left, sno);
+
+	return NULL;
+}
+
+void treeSearchTest()
+{
+    Tree *root = createTree();
+    for (int sno = 1; sno <= 7; sno++) {
+        char *name = treeSearch(root, sno);
+        printf("sno = %d, name = %s\n", sno, name);
+    }
+}
+
+void treeVisitTest()
+{
+    Tree *root = createTree();
+    treeVisit(root);
+}
+
+int main()
+{
+    treeSearchTest();
+	return 0;
+}
